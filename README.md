e923f3297d85b88bab9f3b88f8d6442f2faa8ed9
Glitch (efficacious-resolute-kettledrum) <none>
2020-09-17 18:18:05 +0000

    🏢☺️ Checkpoint

---
+++ README.md
@@ -0,0 +1,38 @@
+# BlyatBot
+
+A Modbot for Discord.  
+Features:  
+1. Kick member \*  
+2. Mass delete messages \*  
+3. Display ones avatar in chat
+4. Detailed help message
+5. Configurable swearword filter  
+6. Timed and permament muting as well as unmuting \*
+7. Temp- and permbans as well as unbanning \*
+8. Warning users + logging who has been warned how many times \*
+9. Non-moderators can report users to admins
+10. Add songs from YouTube to a queue and the bot will play them (->Musicbot)  
+11. Remind you of something if you tell him to  
+12. Create a poll and let the users vote on a yes-no-question
+13. Greet new users via pivate message and give them some info about the server
+
+
+# BlyatBot
+
+Ein Modbot für Discord.  
+Features:  
+1. Member kicken \*
+2. Löschen von bis zu 100 Nachrichten innerhalb von Sekunden \*
+3. Den eigenen Avatar in den Chat posten
+4. Detailierte Infonachricht
+5. Konfigurierbarer Obszönitätenfilter  
+6. Zeitbeschränktes/unbegrenztes Stummschalten von Nutzern sowie wieder Freischalten der Kommunikation \*
+7. Zeitbeschränktes/unbegrenztes Bannen von Nutzern, sowie entbannen durch den Bot \*
+8. Verwarnen von Nutzern + führen eines Warn-Logs \*
+9. Nicht-Moderatoren können Nutzer den Admins melden
+10. Hinzufügen von YouTube-Liedern zu einer Queue und abspielen dieser Queue im Voicechat (->Musicbot)    
+11. Erinnert dich an Dinge, wenn du es ihm befiehlst  
+12. Kann Ja-Nein-Umfragen erstellen und die Servermitglieder abstimmen lassen
+13. Begrüßt neue Nutzer per PM mit einigen Infos über den Server
+
+\* *requires Administrator-Privilege / setzt Administrationsrechte vorraus*
---
+++ index.js
@@ -0,0 +1,151 @@
+﻿const Discord = require("discord.js");
+const config = require("./config.json");
+const fs = require("fs");
+
+var bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+bot.mutes = require("./mutes.json");
+bot.bans = require("./bans.json");
+bot.warns = require("./warns.json");
+bot.remind = require("./reminders.json");
+
+nowPlaying = {};    // used in play.js, skip.js, queue.js
+volume = {};
+servers = {};
+
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) console.error(err);
+
+    let cmdfiles = files.filter(f => f.split(".").pop() === "js");
+    if (cmdfiles.length <= 0) {
+        console.log("No commands to load!");
+        return;
+    }
+
+    console.log(`Loading ${cmdfiles.length} commands!`);
+
+    cmdfiles.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${i + 1}: ${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
+    });
+
+bot.on("guildMemberAdd", (member) => {
+    const guild = member.guild;
+    const embed = new Discord.RichEmbed()
+    .setTitle("Herzlich Willkommen!")
+    .setAuthor("BlyatBot", bot.user.avatarURL)
+    .setColor("#FF0000")
+    .setDescription(`Hallo Genosse ${member.user.username}! Herzlich Willkommen auf dem Server ${guild.name}. Hier ein paar Infos über den Server:`)
+    .setFooter("BlyatBot von Tobinatore", bot.user.avatarURL)
+    .setTimestamp()
+    .addBlankField()
+    .addField("Name",`${guild.name}`, true)
+    .addField("ID", `${guild.id}`, true)
+    .addField("Besitzer",`${guild.owner}`, true)
+    .addField("Mitgliederzahl", `${guild.memberCount}`, true)
+    .addField("Erstelldatum", `${guild.createdAt}`)
+    .addField("Region", `${guild.region}`)
+    .addField("Verifikationslevel", `${guild.verificationLevel}`);
+
+    member.send(embed);
+
+});
+
+
+bot.on("ready",function(){
+    bot.user.setActivity("blyat help", {type:'PLAYING'});
+
+    bot.setInterval(() => {
+        for (let i in bot.mutes) {
+            let time = bot.mutes[i].time;
+            let guildID = bot.mutes[i].guild;
+            let guild = bot.guilds.get(guildID);
+            let member = guild.members.get(i);
+            let mutedRole = guild.roles.find(r => r.name === "BB TimedMuted");
+           // console.log(Date.now());
+            //  console.log(time);
+            console.log(mutedRole.id);
+            console.log("----------------------------------");
+            if (!mutedRole) continue;
+
+            if (Date.now() > bot.mutes[i].time) {
+                console.log(`${member.user.tag}`)
+                member.removeRole(mutedRole);
+                delete bot.mutes[i];
+
+                fs.writeFile("./mutes.json", JSON.stringify(bot.mutes), err=> {
+                    if (err) throw err;
+                    console.log(`${member.user.tag} has been unmuted!`);
+                    member.send(`Genosse, du bist nicht mehr gemuted!`);
+                });
+            }
+        }
+
+        for (let i in bot.bans){
+         let time = bot.bans[i].time;
+         let guildID = bot.bans[i].guild;
+         let guild = bot.guilds.get(guildID);
+         let member = bot.bans[i].user;
+
+        if(!member) continue;
+         if (Date.now() > bot.bans[i].time) {
+                guild.unban(member);
+                delete bot.bans[i];
+
+                fs.writeFile("./bans.json", JSON.stringify(bot.bans), err=> {
+                    if (err) throw err;
+                });
+            }
+        }
+
+         for (let i in bot.remind) {
+            let guild = bot.guilds.get(bot.remind[i].guild);
+            let time = bot.remind[i].time;
+            let member = guild.members.get(i);
+
+            if (Date.now() > bot.remind[i].time) {
+                member.send("Ich sollte dich erinnern, Genosse:\n"+
+                    bot.remind[i].notification);
+                delete bot.remind[i];
+
+                fs.writeFile("./reminders.json", JSON.stringify(bot.remind), err=> {
+                    if (err) throw err;
+                });
+            }
+        }
+
+    }, 3000);
+});
+
+
+bot.on("message", function(message){
+
+	if (message.author.equals(bot.user)) return;
+
+	const swearWords = ["nigger", "faggot","idiot", "bitch", "🇳ℹ🇬🇬🇪🇷", "🇳🇮🇬🇬🇪🇷"];   //you might expand this one
+	if (swearWords.some(word => message.content.toLowerCase().includes(word))) {
+	    message.reply("Zensiert blyat!");   //censored blyat!
+	    message.delete();
+	}
+
+	if(!message.content.startsWith(config.prefix)) return;
+
+	let msgArr = message.content.split(" "); // ["blyat","timedMute","255345474934931456", "10"]  || ["blyat", "tempBan","@Tobinatore", "you've", "failed", "me!", "20" ]
+	let command = msgArr[1]; // "timedMute"  || tempBan
+	let args = msgArr.slice(2, msgArr.length); //["255345474934931456", "10"]   || ["you've", "failed", "me!", "20" ]
+
+	console.log(command);
+
+	let cmd = bot.commands.get(command);
+
+	if (cmd) {
+	    cmd.run(bot, message, args);
+	}
+
+});
+
+
+bot.login(config.token);
